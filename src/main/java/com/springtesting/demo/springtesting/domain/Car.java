8e7f685e43878b7ad18041baf62a6fd61634e0fd
Shridatta <sgz2@njit.edu>
2019-02-13 14:19:48 -0500

    initial commit

---
+++ src/main/java/com/springtesting/demo/springtesting/domain/Car.java
@@ -0,0 +1,17 @@
+package com.springtesting.demo.springtesting.domain;
+
+public class Car {
+    public String type;
+    public String name;
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type){
+         this.type=type;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
